46f22db27d1adc23576ccc3f2181ff3cab344642
Fatih Altınışık <fatih_altinisik@outlook.com>
2025-12-12 07:55:22 +0300

    feat: update app and config names to "Base Mini App Template", add account association, and revise README for template setup.

--- minikit.config.ts
+++ minikit.config.ts
@@ -8,14 +8,20 @@ const ROOT_URL =
  * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
  */
 export const minikitConfig = {
-  
-   
+
+
+  accountAssociation: {
+    header: "eyJmaWQiOjExNDU5NTEsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg2OTJENDI0RkM2NDI2NUU2QjVDMjBjQkFiMjk1Mzk5ZDQ0MzMxNTg2In0",
+    payload: "eyJkb21haW4iOiJuZXctbWluaS1hcHAtcXVpY2tzdGFydC1pdm9yeS1zaXgudmVyY2VsLmFwcCJ9",
+    signature: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAmAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB6brkOez3HeZgTfZvsNhWEqluwFIWXie5CcCLVp6ttssLRnxekUJpqz4btZjVCuY659CnKXAvN5ZxdZ4MJlEjFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAl8ZgIay2xclZzG8RWZzuWvO8j9R0fus3XxDee9lRlVy8dAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD3eyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoid0xuOGpGTGN0YTNSWUNnYjk3VmpMY3FZTURQU05RRWJwVmUxWnNJMVNQMCIsIm9yaWdpbiI6Imh0dHBzOi8va2V5cy5jb2luYmFzZS5jb20iLCJjcm9zc09yaWdpbiI6ZmFsc2UsIm90aGVyX2tleXNfY2FuX2JlX2FkZGVkX2hlcmUiOiJkbyBub3QgY29tcGFyZSBjbGllbnREYXRhSlNPTiBhZ2FpbnN0IGEgdGVtcGxhdGUuIFNlZSBodHRwczovL2dvby5nbC95YWJQZXgifQAAAAAAAAAAAA"
+  },
+
 
   miniapp: {
     version: "1",
-    name: "Mini App Quickstart Template", 
-    subtitle: "Quickstart Template", 
-    description: "A starter template for building Farcaster Mini Apps using Next.js. By Trio Blockchain Labs.",
+    name: "Mini App Quickstart Template",
+    subtitle: "Quickstart Template",
+    description: "A starter template for building Base Mini Apps using Next.js. By Trio Blockchain Labs.",
     screenshotUrls: [],
     iconUrl: `${ROOT_URL}/blue-icon.png`,
     splashImageUrl: `${ROOT_URL}/blue-hero.png`,
@@ -23,11 +29,11 @@ export const minikitConfig = {
     homeUrl: ROOT_URL,
     webhookUrl: `${ROOT_URL}/api/webhook`,
     primaryCategory: "developer-tools",
-    tags: ["developer-tools","productivity"],
-    heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
+    tags: ["developer-tools", "productivity"],
+    heroImageUrl: `${ROOT_URL}/blue-hero.png`,
     tagline: "Ship mini apps faster. By TriO",
     ogTitle: "Mini App Quickstart Template",
-    ogDescription: "A template for building Farcaster Mini Apps using Next.js and TypeScript. By Trio Blockchain Labs",
+    ogDescription: "A template for building Base Mini Apps using Next.js and TypeScript. By Trio Blockchain Labs",
     ogImageUrl: `${ROOT_URL}/blue-hero.png`,
   },
 } as const;
